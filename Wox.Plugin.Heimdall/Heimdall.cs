1806d1b9eaf43a7857115542aa555473a1f3c218
Dominic Hock <subtixx@gmail.com>
2021-09-04 13:58:06 +0200

    Add license

--- Wox.Plugin.Heimdall/Heimdall.cs
+++ Wox.Plugin.Heimdall/Heimdall.cs
@@ -1,3 +1,20 @@
+/*
+    Wox.Plugin.Heimdall - A Heimdall integration for wox
+    Copyright (C) 2021 Dominic Hock
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,13 +25,13 @@ namespace Wox.Plugin.Heimdall
 {
     public static class Heimdall
     {
-        public static List<HeimdallApp> RequestAppList2(string url, string folderDir)
+        public static List<HeimdallApp> RequestAppList(string url, string folderDir)
         {
             try
             {
                 var uri = new Uri(url);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return new List<HeimdallApp>();
             }
--- Wox.Plugin.Heimdall/Main.cs
+++ Wox.Plugin.Heimdall/Main.cs
@@ -1,4 +1,22 @@
-﻿using System.Collections.Generic;
+﻿/*
+    Wox.Plugin.Heimdall - A Heimdall integration for wox
+    Copyright (C) 2021 Dominic Hock
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -35,7 +53,7 @@ namespace Wox.Plugin.Heimdall
 
             if (_settings.HeimdallUrl == "") return;
 
-            _appList = Heimdall.RequestAppList2(_settings.HeimdallUrl, ImagesDirectory);
+            _appList = Heimdall.RequestAppList(_settings.HeimdallUrl, ImagesDirectory);
             foreach (var heimdallApp in _appList)
             {
                 _settings.HeimdallApps.Add(heimdallApp);
@@ -84,7 +102,7 @@ namespace Wox.Plugin.Heimdall
                 Score = -9999,
                 Action = e =>
                 {
-                    _appList = Heimdall.RequestAppList2(_settings.HeimdallUrl, ImagesDirectory);
+                    _appList = Heimdall.RequestAppList(_settings.HeimdallUrl, ImagesDirectory);
                     return true;
                 }
             });
